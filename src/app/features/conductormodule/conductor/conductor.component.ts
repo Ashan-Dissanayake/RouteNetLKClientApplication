bea04659fa91e0260b060068541a0be8a73a2a72
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-12-31 12:58:59 +0530

    initial commit

---
+++ src/app/features/conductormodule/conductor/conductor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conductor',
+  imports: [],
+  templateUrl: './conductor.component.html',
+  styleUrl: './conductor.component.scss'
+})
+export class ConductorComponent {
+
+}
